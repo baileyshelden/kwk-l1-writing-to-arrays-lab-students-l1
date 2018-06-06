b0e75b9db71297b0fcc9561fa1ce06be844423e0
baileyshelden <bailey7748@gmail.com>
2018-06-06 21:49:20 +0000

    Done.

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,13 +1,26 @@
-rainbow_colors = [
-  "yellow",
-  "default",
-  "light_cyan"
-]
+# rainbow_colors = 
+#   [ "yellow",
+#   "default",
+#   "light_cyan" ]
 
 def change_rainbow_colors
+  rainbow_colors = []
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+change_rainbow_colors[0]= "red"
+change_rainbow_colors[1]= "light_red"
+change_rainbow_colors[2]= "light_yellow"
+return change_rainbow_colors
 end
 
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-end
+puts change_rainbow_colors
+
+
+
+# def add_colors
+#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+#   change_rainbow_colors << "Green"
+# change_rainbow_colors << "Blue"
+# end
+
+
+# add_colors
---
+++ sandbox.rb
@@ -0,0 +1,16 @@
+
+
+puts "What is your name?"
+name_length_random = gets.chomp.length
+def name_length(name_length)
+  
+if name_length <= 3
+  puts "your name is shoooooort"
+elsif name_length <= 7
+puts "you, have an average name."
+else name_length > 7
+  puts "your name is lonnnnnng as heck."
+end 
+end 
+
+name_length(name_length_random)
