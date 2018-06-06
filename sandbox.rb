beb5ec0cb7adeaf0bef13008d51a5cd004b22131
baileyshelden <bailey7748@gmail.com>
2018-06-06 21:32:13 +0000

    automatically backed up by learn

--- sandbox.rb
+++ sandbox.rb
@@ -1,11 +1,11 @@
 def name_length(x)
 x = gets.chomp 
 puts "How long is your name?"
-x = gets.chomp.to_i 
+x = gets.chomp.to_length
 if name_length =< 3
   puts "your name is shoooooort"
-elsif name_length =< 7
-puts "you, have a average name."
+elsif name_length >= 7
+puts "you, have an average name."
 else 
   puts "your name is lonnnnnng as heck."
 end 
